cd382e1f0b0148f62b2ff01a5e70922ec154bc16
back-xy <bawarmohamad100@gmail.com>
2023-02-04 04:30:12 +0300

    changing fetch url to render

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,13 +21,16 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8000/api/v1/posts", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ ...form }),
-        });
+        const response = await fetch(
+          "https://ai-art-jjuw.onrender.com/api/v1/posts",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ ...form }),
+          }
+        );
 
         await response.json();
         navigate("/");
@@ -51,13 +54,16 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImage(true);
-        const response = await fetch("http://localhost:8000/api/v1/dalle", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ prompt: form.prompt }),
-        });
+        const response = await fetch(
+          "https://ai-art-jjuw.onrender.com/api/v1/dalle",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt: form.prompt }),
+          }
+        );
         const data = await response.json();
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
       } catch (error) {
