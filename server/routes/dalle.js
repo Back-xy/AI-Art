a807be029eb42884fcd0b819da2aa0b7cbf0eef2
back-xy <bawarmohamad100@gmail.com>
2023-08-20 14:09:14 +0300

    should be it

--- server/routes/dalle.js
+++ server/routes/dalle.js
@@ -17,20 +17,13 @@ router
     try {
       const { prompt } = req.body;
 
-      // const aiResponse = await openai.createImage({
-      //   prompt,
-      //   n: 1,
-      //   size: '1024x1024',
-      //   response_format: 'b64_json',
-      // });
-      // const img = aiResponse.data.data[0].b64_json;
-
-      const response = await openai.createImage({
-        prompt: 'a white siamese cat',
+      const aiResponse = await openai.createImage({
+        prompt,
         n: 1,
         size: '1024x1024',
+        response_format: 'b64_json',
       });
-      img = response.data.data[0].url;
+      const img = aiResponse.data.data[0].b64_json;
 
       res.status(200).json({ photo: img });
     } catch (error) {
